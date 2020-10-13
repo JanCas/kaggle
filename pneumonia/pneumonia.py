6c990b1aeaba8ec725d1375009d72ca4833536ec
JanCas <jan.l.cas@outlook.com>
2020-10-13 14:37:12 -0600

    smaller batch

--- pneumonia/pneumonia.py
+++ pneumonia/pneumonia.py
@@ -1,25 +1,33 @@
-
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.optimizers import SGD
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
 from tensorflow.keras.models import Sequential
+from tensorflow.keras.metrics import Recall, Precision
+from tensorflow.keras.callbacks import EarlyStopping, TensorBoard
+from tensorflow.compat.v1 import ConfigProto, Session
 import tensorflow as tf
+
 from os import getcwd
 
-config = tf.compat.v1.ConfigProto()
+config = ConfigProto()
 config.gpu_options.allow_growth = True
-session = tf.compat.v1.Session(config=config)
+session = Session(config=config)
 
 BASE_DIR = getcwd()
-#%%
+# %%
 
-datagen = ImageDataGenerator(rescale=1./255, dtype=tf.dtypes.float16)
-train_generator = datagen.flow_from_directory(BASE_DIR + '\\chest_xray\\train\\', target_size=(227, 227), batch_size=128, color_mode='grayscale')
-test_generator = datagen.flow_from_directory(BASE_DIR+ '\\chest_xray\\test\\', target_size=(227, 227), batch_size=128, color_mode='grayscale')
-val_generator = datagen.flow_from_directory(BASE_DIR+ '\\chest_xray\\val\\', target_size=(227, 227), batch_size=16, color_mode='grayscale')
+datagen = ImageDataGenerator(rescale=1. / 255, dtype=tf.dtypes.float16)
+train_generator = datagen.flow_from_directory(BASE_DIR + '\\chest_xray\\train\\', target_size=(227, 227), batch_size=16,
+                                              color_mode='grayscale')
+test_generator = datagen.flow_from_directory(BASE_DIR + '\\chest_xray\\test\\', target_size=(227, 227), batch_size=128,
+                                             color_mode='grayscale')
+val_generator = datagen.flow_from_directory(BASE_DIR + '\\chest_xray\\val\\', target_size=(227, 227), batch_size=16,
+                                            color_mode='grayscale')
 
-#%%
 
-#AlexNet
+# %%
+
+# AlexNet
 def AlexNet(weights=None):
     '''
     keras implementation of AlexNet
@@ -28,21 +36,21 @@ def AlexNet(weights=None):
     '''
     model = Sequential()
 
-    #1ST layer
-    model.add(Conv2D(filters=96, strides=4, input_shape=(227,227,1), kernel_size=11, activation='relu'))
+    # 1ST layer
+    model.add(Conv2D(filters=96, strides=4, input_shape=(227, 227, 1), kernel_size=11, activation='relu'))
     model.add(MaxPooling2D(pool_size=3, strides=2))
 
-    #2nd layer
+    # 2nd layer
     model.add(Conv2D(filters=256, padding='same', kernel_size=3, activation='relu'))
     model.add(MaxPooling2D(pool_size=3, strides=2))
 
-    #3rd layer
+    # 3rd layer
     model.add(Conv2D(filters=384, padding='same', kernel_size=5, activation='relu'))
 
-    #4th layer
+    # 4th layer
     model.add(Conv2D(filters=384, padding='same', kernel_size=3, activation='relu'))
 
-    #5th layer
+    # 5th layer
     model.add(Conv2D(filters=256, padding='same', kernel_size=3, activation='relu'))
     model.add(MaxPooling2D(pool_size=3, strides=2))
 
@@ -55,7 +63,7 @@ def AlexNet(weights=None):
     model.add(Dropout(.5))
     model.add(Dense(4096, activation='relu'))
 
-    #output layer
+    # output layer
     model.add(Dense(2, activation='sigmoid'))
 
     if weights is not None:
@@ -63,19 +71,34 @@ def AlexNet(weights=None):
 
     return model
 
-#%%
 
-#training the model
+# %%
+
+# training the model
 model = AlexNet()
 
-model.compile(loss='binary_crossentropy', optimizer=tf.keras.optimizers.SGD(learning_rate=0.01), metrics=['accuracy'])
+optimizer = SGD(lr=.01)
+METRICS = [
+    'accuracy',
+    Precision(name='precision'),
+    Recall(name='recall')
+]
+
+log_dir = "logs/fit/"
+callbacks = [
+    EarlyStopping(monitor='loss', patience=3),
+    #TensorBoard(log_dir=log_dir, histogram_freq=1)
+]
+
+model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=METRICS)
 model.summary()
 
 model.fit(
     train_generator,
-    epochs=20,
-    steps_per_epoch=40,
+    epochs=50,
+    steps_per_epoch=320,
     validation_data=val_generator,
+    callbacks=callbacks
 )
 
 model.evaluate(test_generator)
